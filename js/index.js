9e655c2a8733987b5c7d46abc379020fdef2bf47
ziaulsiddiqui <ziauls42@gmail.com>
2020-04-12 22:21:33 +0530

    First Commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+//let zipCode = prompt("Enter Zip Code");
+//document.getElementById('zip-code').innerHTML = zipCode;
+function initMap() {
+    var sydney = {lat: -33.863276, lng: 151.107977};
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: sydney,
+      zoom: 11,
+      mapTypeId: 'roadmap',            
+    });       
+  }
---
+++ todo.md
@@ -0,0 +1,16 @@
+# Plan Of Action
+
+- Create Wireframe    DONE
+
+- Setup the base HTML structure DONE
+
+- Import Google Mpas into the window  DONE
+
+- Add the text Store Locator DONE
+
+- Add input box DONE
+
+- Add Store List Container
+
+- Add individual store container
+
